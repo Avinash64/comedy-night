f3006204b8c3389bc57beb9ac761dcff4b8f4d40
n-type <you@example.com>
2023-03-05 22:45:55 -0500

    fastapi_added

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,34 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    roster: list
+    id: int | None = None
+
 
 posts = {}
 
 def add_post(item):
-    posts[len(list(posts.keys()))+1]
+    id_ = len(list(posts.keys()))+1
+    posts[id_] = {
+        "id" : len(list(posts.keys()))+1,
+        "list" : item
+    }
+    return posts[id_]
 
+def get_post(id):
+    return posts[id] 
+
+def update_post(id, item):
+    posts[id] = {
+        "id" : id,
+        "list" : item
+    }
+    return posts[id]
+
+def delete_post(id):
+    del posts[id]
 
 app = FastAPI()
 
@@ -12,3 +36,21 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return get_post(item_id)
+
+@app.post("/items/")
+async def create_item(item: Item):
+    print(item)
+    return add_post(item.roster)
+    
+
+# @app.post("/items/")
+# async def create_item(item: Item):
+#     return item
+# @app.post("/items/")
+# async def create_item(item: Item):
+#     return item
